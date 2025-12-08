ec5ee8c0f6bdf54a1a1a333ab8397335a879a431
joyboy5477 <joyboy5477@users.noreply.github.com>
2025-12-08 05:45:20 +0000

    Deploying to gh-pages from @ iclr-blogposts/2026@76e8544bc8b86615b2ad9607d034f1c78a87bdd2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -281,17 +281,6 @@ ninja.data = [{
             window.location.href = "/2026/blog/2026/subject-invariant-eeg/";
           
         },
-      },{id: "post-artistic-style-and-the-play-of-neural-style-representations",
-        
-          title: "Artistic Style and the Play of Neural Style Representations",
-        
-        description: "How do neural networks percieve the complex human construct of artistic style? We explore the dynamic interplay between diverse machine representations of style and style definitions. We reveal a profound divergence where models often reject established historical narratives in favour of their own perceptual truths.",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/2026/blog/2026/style-representations/";
-          
-        },
       },{id: "post-speeding-up-training-of-model-free-reinforcement-learning-a-comparative-evaluation-for-fast-and-accurate-learning",
         
           title: "Speeding up Training of Model-Free Reinforcement Learning :A Comparative Evaluation for Fast and...",
