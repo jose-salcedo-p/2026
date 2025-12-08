919bb892a3ae6e5a5ef9e77e49d922b53c85ce55
maxwshen <maxwshen@users.noreply.github.com>
2025-12-08 07:51:26 +0000

    Deploying to gh-pages from @ iclr-blogposts/2026@76e8544bc8b86615b2ad9607d034f1c78a87bdd2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,6 +391,17 @@ ninja.data = [{
             window.location.href = "/2026/blog/2026/scaling-rlvr/";
           
         },
+      },{id: "post-trilemma-for-data-free-sampler-evaluation",
+        
+          title: "Trilemma for data-free sampler evaluation",
+        
+        description: "Neural samplers like diffusion samplers aim to learn to sample a target unnormalized energy potential, which is known but considered challenging to sample from. Here, we prove an impossible trinity (trilemma) for data-free sampler evaluation; we can only have two among i) mode-covering metric, ii) stable with finite variance, iii) no dominance cycles. We situate the implications of this trilemma in the broader conceptual landscape of data-driven and data-free sampler evaluation.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/2026/blog/2026/sampler-eval-trilemma/";
+          
+        },
       },{id: "post-getting-sac-to-work-on-a-massive-parallel-simulator-an-rl-journey-with-off-policy-algorithms",
         
           title: "Getting SAC to Work on a Massive Parallel Simulator: An RL Journey With...",
