8071e20c51f6315d8e062ace9ba666b33ebb643f
frbm <frbm@users.noreply.github.com>
2026-03-16 18:10:27 +0000

    Deploying to gh-pages from @ iclr-blogposts/2026@e3358f3f446a21cbabb0e6f10ba8c3c78883d460 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1326,17 +1326,6 @@ ninja.data = [{
             window.location.href = "/2026/blog/2026/a-human-centric-framework-for-debating-the-ethics-of-ai-consciousness-under-uncertainty/";
           
         },
-      },{id: "post-effect-of-parallel-environments-and-rollout-steps-in-ppo",
-        
-          title: "Effect of Parallel Environments and Rollout Steps in PPO",
-        
-        description: "This blog post explores batch size in PPO-what happens when we increase the number of parallel environments versus the number of rollout steps, while keeping the total samples per update fixed. We discuss how this affects bias and variance in gradient estimation.",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/2026/blog/2026/ppo-batch-size/";
-          
-        },
       },{id: "post-",
         
           title: "",
